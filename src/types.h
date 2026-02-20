54140dac7527f3585c1cb8c9a32c213bbb5d66de
nrmayer <noahrmayer@gmail.com>
2026-02-20 12:48:23 -0700

    Add GPIO pin typing

---
+++ src/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include "stm32f072xb.h" // GPIOA, B, C, etc
+#include "stm32f0xx_hal.h" // HAL_*()
+
+typedef struct GPIO_Pin {
+    GPIO_TypeDef* letter;
+    uint16_t num;
+} GPIO_Pin;
+
+typedef uint8_t KeyCode;
+
+#endif
